ebe6133057e39e4f03b591be6372db962f335d9b
sobolevn <mail@sobolevn.me>
2021-06-17 19:15:06 +0300

    Adds more tests

---
+++ tests/test_typeclass/test_repr.py
@@ -0,0 +1,23 @@
+from classes import AssociatedType, typeclass
+
+
+class MyType(AssociatedType):
+    """Docs for type."""
+
+
+@typeclass(MyType)
+def my_typeclass_with_type(instance) -> str:
+    """Docs."""
+
+
+@typeclass
+def my_typeclass(instance) -> str:
+    """Docs."""
+
+
+def test_str():
+    """Ensures that ``str`` is correct."""
+    assert str(my_typeclass) == '<typeclass "my_typeclass">'
+    assert str(
+        my_typeclass_with_type,
+    ) == '<typeclass "my_typeclass_with_type": "MyType">'
