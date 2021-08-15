09d5a2b69c70743915017cd5f7f74773577df30a
sobolevn <mail@sobolevn.me>
2021-08-15 16:15:32 +0300

    Fixes CI

--- tests/test_typeclass/test_typed_dict.py
+++ tests/test_typeclass/test_typed_dict.py
@@ -1,4 +1,5 @@
 import sys
+from typing import Any
 
 import pytest
 from typing_extensions import TypedDict
@@ -12,9 +13,9 @@ else:
         name: str
         registered: bool
 
-    _TypedDictMeta = type(TypedDict)
+    _TypedDictMeta: Any = type(TypedDict)
 
-    class _UserDictMeta(_TypedDictMeta):  # type: ignore
+    class _UserDictMeta(_TypedDictMeta):
         def __instancecheck__(self, arg: object) -> bool:
             return (
                 isinstance(arg, dict) and
@@ -33,7 +34,19 @@ else:
     def _get_name_user_dict(instance: UserDict) -> str:
         return instance['name']
 
-    def test_typed_dict():
+    def test_correct_typed_dict():
         """Ensures that typed dict dispatch works."""
         user: UserDict = {'name': 'sobolevn', 'registered': True}
         assert get_name(user) == 'sobolevn'
+
+    @pytest.mark.parametrize('test_value', [
+        [],
+        {},
+        {'name': 'sobolevn', 'registered': None},
+        {'name': 'sobolevn'},
+        {'registered': True},
+    ])
+    def test_wrong_typed_dict(test_value):
+        """Ensures that typed dict dispatch works."""
+        with pytest.raises(NotImplementedError):
+            get_name(test_value)
